7d61571fdab836ea39bdba464000588c2c428e73
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2023-09-13 23:48:24 +0600

    openAi_api_added
    open ai api  key added and responses are shown

--- myenv/ChatApp/chat/views.py
+++ myenv/ChatApp/chat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from chat.models import Room, Message
+from .models import Room, Message
 from django.http import HttpResponse, JsonResponse
-
+import openai
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -40,3 +40,23 @@ def getMessages(request, room):
 
     messages = Message.objects.filter(room=room_details.id)
     return JsonResponse({"messages":list(messages.values())})
+
+def getResponse(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+    openai.api_key = "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"
+    # Call the ChatGPT API to generate a response
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # Specify the ChatGPT engine
+        prompt=f"You are in room {room_id}. User {username}: {message}\n",
+        max_tokens=50,  # Adjust this based on your desired response length
+    )
+
+    generated_message = response.choices[0].text.strip()
+
+    # Create a new Message object with the generated response
+    new_message = Message.objects.create(value=generated_message, user="ChatGPT", room=room_id)
+    new_message.save()
+    
+    return HttpResponse(generated_message)
